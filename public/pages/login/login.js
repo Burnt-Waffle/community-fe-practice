02f0fa9b5ca393ee7a27bf46d8aeff002be65bc3
Burnt-Waffle <michael568@hanmail.net>
2025-11-16 12:25:06 +0900

    fix: change terms endpoint

--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -11,7 +11,7 @@ const signupLink = document.getElementById('signup-link');
 // DOM이 완전히 로드된 후에 스크립트를 실행
 document.addEventListener('DOMContentLoaded', async () => {
     await loadHeader({ showProfileButton: false, showBackButton: false });
-    signupLink.href = `${API_BASE_URL}/api/terms`;
+    signupLink.href = `${API_BASE_URL}/terms`;
 });
 
 // 로그인 버튼의 이벤트 리스너
