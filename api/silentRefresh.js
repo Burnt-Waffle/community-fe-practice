8bdb97a5c65ff7e63eed30f348c198023994f332
Burnt-Waffle <michael568@hanmail.net>
2025-11-16 11:22:25 +0900

    rename: Move files to utils from api to solve load balancer route confilct

--- api/silentRefresh.js
+++
@@ -1,9 +0,0 @@
-import { refreshToken } from "./authClient.js";
-
-export const performSilentRefresh = async () => {
-    try {
-        await refreshToken();
-    } catch (error) {
-        console.log("Silent Refresh failed.")
-    }
-}
