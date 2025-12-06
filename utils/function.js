7c5c941e6f207cb235adf19a0d587b2c8f2e1106
Burnt-Waffle <michael568@hanmail.net>
2025-12-06 13:09:38 +0900

    feat: change every timezone to kst

--- utils/function.js
+++ utils/function.js
@@ -1,11 +1,15 @@
 export const formatDate = (date) => {
     const now = new Date(date)
-    const year = now.getFullYear()
-    const month = String(now.getMonth() + 1).padStart(2, '0')
-    const day = String(now.getDate()).padStart(2, '0')
-    const hours = String(now.getHours()).padStart(2, '0')
-    const minutes = String(now.getMinutes()).padStart(2, '0')
-    const seconds = String(now.getSeconds()).padStart(2, '0')
+    const utc = inputDate.getTime() + (inputDate.getTimezoneOffset() * 60 * 1000);
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    const kstDate = new Date(utc + KR_TIME_DIFF);
+
+    const year = kstDate.getFullYear()
+    const month = String(kstDate.getMonth() + 1).padStart(2, '0')
+    const day = String(kstDate.getDate()).padStart(2, '0')
+    const hours = String(kstDate.getHours()).padStart(2, '0')
+    const minutes = String(kstDate.getMinutes()).padStart(2, '0')
+    const seconds = String(kstDate.getSeconds()).padStart(2, '0')
 
     const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
     return formattedDate
